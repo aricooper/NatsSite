1e034db403ca84eb424e8ac9cc546934af449029
Node Coding Exercise <arikorycooper@gmail.com>
2018-03-13 13:05:43 -0700

    TIDAL API set up
    need to set up music with videos still

--- public/javascripts/vidOnClick.js
+++ public/javascripts/vidOnClick.js
@@ -1,37 +1,38 @@
-
-
-videoPlayer.addEventListener('click', function () {
-    startMusic();
-    if (videoPlayer.paused === false) {
-        videoPlayer.pause();
-        videoPlayer.firstChild.nodeValue = 'Play';
-    } else {
-        videoPlayer.play();
-        videoPlayer.firstChild.nodeValue = 'Pause';
-    }
-});
-videoPlayer2.addEventListener('click', function () {
-    if (videoPlayer2.paused === false) {
-        videoPlayer2.pause();
-        videoPlayer2.firstChild.nodeValue = 'Play';
-    } else {
-        videoPlayer2.play();
-        videoPlayer2.firstChild.nodeValue = 'Pause';
-    }
-});
-
 var TidalAPI = require('tidalapi');
 
 var api = new TidalAPI({
     username: 'nzedan@willamette.edu',
     password: 'supercali',
-    token: 'S0wuxKXcRQLsoQ6R',
+    token: '4zx46pyr9o8qZNRw',
     // Could also be 'LOSSLESS' but this only supported on premium subscriptions
     quality: 'HIGH'
 });
+startMusic();
+
+//videoPlayer.addEventListener('click', function () {
+//    startMusic();
+//    if (videoPlayer.paused === false) {
+//        videoPlayer.pause();
+//        videoPlayer.firstChild.nodeValue = 'Play';
+//    } else {
+//        videoPlayer.play();
+//        videoPlayer.firstChild.nodeValue = 'Pause';
+//    }
+//});
+//videoPlayer2.addEventListener('click', function () {
+//    if (videoPlayer2.paused === false) {
+//        videoPlayer2.pause();
+//        videoPlayer2.firstChild.nodeValue = 'Play';
+//    } else {
+//        videoPlayer2.play();
+//        videoPlayer2.firstChild.nodeValue = 'Pause';
+//    }
+//});
+
 
 function startMusic() {
+    console.log('Tidal:  \n');
     api.search({type: 'artists', query: 'Dream Theater', limit: 1}, function (data) {
-            console.log(data.artists);
-        });
+        console.log(data.artists);
+    });
 }
