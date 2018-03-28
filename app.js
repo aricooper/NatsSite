6bb3996aa6ecbf719cf48e58f36ab57f678a8277
Node Coding Exercise <arikorycooper@gmail.com>
2018-03-28 13:54:26 -0700

    more changes for css

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ function compile(str, path) {
     .use(nib())
 }
 // set up port
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || 'localhost';
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
